5798223df5eabc1d79e8d7172e4061218a91f9a0
Will Haight <willhaight121@gmail.com>
2021-05-05 20:35:45 -0600

    jQuery Practice

---
+++ app.js
@@ -0,0 +1,37 @@
+$('.navItem').css('color','white' );
+$('#nav').css('background-color', '#145');
+
+let in1 = $('ul').children()[0];
+$(in1).css('background-color', '#ccc');
+
+let in2 = $('ul').children()[2];
+$(in2).css('background-color', '#ccc');
+
+let in3 = $('ul').children()[4];
+$(in3).css('background-color', '#ccc');
+
+let in4 = $('ul').children()[6];
+$(in4).css('background-color', '#ccc');
+
+let in5 = $('ul').children()[8];
+$(in5).css('background-color', '#ccc');
+
+let in6 = $('ul').children()[10];
+$(in6).css('background-color', '#ccc');
+
+let in7 = $('ul').children()[1];
+$(in7).css('background-color', '#aaa');
+
+let in8 = $('ul').children()[3];
+$(in8).css('background-color', '#aaa');
+
+let in9 = $('ul').children()[5];
+$(in9).css('background-color', '#aaa');
+
+let in10 = $('ul').children()[7];
+$(in10).css('background-color', '#aaa');
+
+let in11 = $('ul').children()[9];
+$(in11).css('background-color', '#aaa');
+
+$('#myInput').attr('placeholder', 'Will Haight');
